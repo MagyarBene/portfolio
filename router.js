21f439f424a0b82f654eeaf1302636c648fa2012
hszdavid971 <hszdavid990@gmail.com>
2024-02-02 14:16:02 +0100

    'portfolio'

---
+++ router.js
@@ -0,0 +1,24 @@
+const routes={
+    "home":"home.html",
+    "about":"about.html",
+    "projects":"projects.html",
+    "contact":"contact.html"
+}
+window.addEventListener('hashchange',()=>{
+    //console.log(window.location.hash);
+    navigate()
+})
+
+function navigate(){
+    let hash=window.location.hash.substring(2)
+    console.log(hash);
+    let page = routes[hash]
+    loadPage(page,document.querySelector('main'))
+
+}
+async function loadPage(url,domObj) {
+    const response = await fetch(url)
+    const html = await response.text()
+    domObj.innerHTML=html
+    if(url=='about.html') about()
+}
